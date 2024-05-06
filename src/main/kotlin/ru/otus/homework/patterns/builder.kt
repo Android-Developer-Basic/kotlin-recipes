6eaadab537cdcc6b6220c614262129415523d366
anastasiashchegoleva <a.shchegoleva88@mail.ru>
2024-05-06 23:15:24 +0300

    HomeWork5 fixed builder

--- src/main/kotlin/ru/otus/homework/patterns/builder.kt
+++ src/main/kotlin/ru/otus/homework/patterns/builder.kt
@@ -1,47 +1,39 @@
 package ru.otus.homework.patterns
 
 fun main () {
-    val myperson = PersonBuilder()
-    myperson.withSurname("Петров").withName("Петя").withBirthPlace("Рим")
+    val myperson = Person.Builder()
+        .withSurname("Петров").withName("Петя").withBirthPlace("Рим").build()
     println(myperson.toString())
 
-    val myperson2 = PersonBuilder()
-    myperson2.withSurname("Иванов").withAge(35)
+    val myperson2 = Person.Builder()
+        .withSurname("Иванов").withAge(35).build()
     println(myperson2.toString())
 }
 
-class PersonBuilder {
-    private var name = ""
-    private var surname = ""
-    private var age = 0
-    private var birthPlace = ""
-
-    fun withName (name: String): PersonBuilder {
-        this.name = name
-        return this
+class Person private constructor(
+    private val name: String?,
+    private val surname: String?,
+    private val age: Int?,
+    private val birthPlace: String?
+){
+    data class Builder (
+        var name: String? = null,
+        var surname: String? = null,
+        var age: Int? = null,
+        var birthPlace: String? = null
+    ) {
+        fun withName(name: String) = apply { this.name = name }
+        fun withSurname(surname: String) = apply { this.surname = surname }
+        fun withAge(age: Int) = apply {this.age = age}
+        fun withBirthPlace(birthPlace: String) = apply {this.birthPlace = birthPlace}
+        fun build ():Person = Person(name,surname,age,birthPlace)
     }
-
-    fun withSurname (surname: String): PersonBuilder {
-        this.surname = surname
-        return this
-    }
-
-    fun withAge (age: Int): PersonBuilder {
-        this.age = age
-        return this
-    }
-
-    fun withBirthPlace (birthPlace: String): PersonBuilder {
-        this.birthPlace = birthPlace
-        return this
-    }
-
     override fun toString(): String {
         var result = ""
-        if (name != "") result += "Имя: $name "
-        if (surname != "") result += "Фамилия: $surname "
-        if (age != 0) result += "Возраст: $age "
-        if (birthPlace != "") result += "Место рождения: $birthPlace "
+        if (name != null) result += "Имя: $name "
+        if (surname != null) result += "Фамилия: $surname "
+        if (age != null) result += "Возраст: $age "
+        if (birthPlace != null) result += "Место рождения: $birthPlace "
         return result
     }
 }
