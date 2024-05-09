b7aafda5de9e085d52f9e5de757fcbc381fdba8d
nikolay <nazarov.nikolay15@wb.ru>
2024-05-10 00:06:53 +0300

    command поправил

--- src/main/kotlin/ru/otus/homework/homeworkpatterns/command.kt
+++ src/main/kotlin/ru/otus/homework/homeworkpatterns/command.kt
@@ -2,29 +2,54 @@ package ru.otus.homework.homeworkpatterns
 
 
 fun main() {
-    val textView = TextViewText()
+    val textView = TextView()
     println("В текствью сейчас такой текст: ${textView.getText()}")
     textView.add("Привет")
     println("В текствью сейчас такой текст: ${textView.getText()}")
     textView.add(" мир!")
     println("В текствью сейчас такой текст: ${textView.getText()}")
-
-
 }
 
-class TextViewText {
+class TextView {
     private var text: String = ""
+    private var history: List<Command> = emptyList()
+    private var historyPointer = 0
     fun getText(): String {
         return text
     }
 
-    fun add(textToAdd: String): TextViewText = apply {
-        AddText(textToAdd).execute()
+    private fun addCommand(command: Command) {
+        history = history.subList(0, historyPointer) + command.apply { execute() }
+        historyPointer++
     }
-    private inner class AddText(private val textToAdd: String) {
-        fun execute() {
+
+
+    fun add(textToAdd: String): TextView = apply {
+        addCommand(AddText(textToAdd))
+    }
+    private inner class AddText(private val textToAdd: String) : Command {
+        override fun execute() {
             text += textToAdd
         }
+
+        override fun undo() {
+            text = text.replace(textToAdd, "")
+        }
+
+    }
+
+    interface Command {
+        fun execute()
+        fun undo()
+    }
+
+    fun undo():
+            TextView = apply {
+        history.getOrNull(--historyPointer)?.undo()
+    }
+    fun redo():
+            TextView = apply {
+        history.getOrNull(historyPointer++)?.execute()
     }
 }
 
