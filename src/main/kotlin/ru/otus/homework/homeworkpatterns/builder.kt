eeb0cb42c8d0ca70d1f471aec0825d7c0387a57a
nikolay <nazarov.nikolay15@wb.ru>
2024-05-12 14:24:31 +0300

    Поправил builder

--- src/main/kotlin/ru/otus/homework/homeworkpatterns/builder.kt
+++ src/main/kotlin/ru/otus/homework/homeworkpatterns/builder.kt
@@ -1,35 +1,31 @@
 package ru.otus.homework.homeworkpatterns
 
-data class User(val name: String, val age: Int, val email: String) {
-    class Builder {
-        private var name: String = ""
-        private var age: Int = 0
-        private var email: String = ""
+class User private constructor(val name: String?, val age: Int?, val email: String?) {
+    override fun toString(): String {
+        return "User(name=$name, age=$age, email=$email)"
+    }
+    data class Builder(
+        var name: String? = null,
+        var age: Int? = null,
+        var email: String? = null
+    )
+    {
+        fun name (name: String) = apply {this.name = name}
+        fun age (age: Int) = apply {this.age = age}
+        fun email (email: String) = apply {this.email = email}
 
-        fun setName(name: String) {
-            this.name = name
-        }
+        fun build() = User(name, age, email)
 
-        fun setAge(age: Int) {
-            this.age = age
-        }
 
-        fun setEmail(email: String) {
-            this.email = email
-        }
-
-        override fun toString(): String {
-            return "User(name=$name, age=$age, email=$email)"
-        }
     }
 }
 
 fun main() {
-    val user = User.Builder().apply {
-        setName("Alice")
-        setAge(30)
-        setEmail("alice@example.com")
-    }
-
+    // Я сильно запутался и решил взять просто как пример код в сайта
+    // https://www.baeldung.com/kotlin/builder-pattern
+    val user = User.Builder().name("John").age(30).email("john@mail.ru").build()
     println(user)
+    // А вот так теперь не работает
+    //val user1 = User("Alexey", 16, "alex@mail.ru")
+
 }
