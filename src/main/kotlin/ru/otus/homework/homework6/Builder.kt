3e93397f8df4e898640ffa231ab4f7acd2c47f2c
PavelSkobeev <pavel_skobeev@mail.ru>
2024-06-04 13:01:10 +0300

    change Singleton and Builder

--- src/main/kotlin/ru/otus/homework/homework6/Builder.kt
+++ src/main/kotlin/ru/otus/homework/homework6/Builder.kt
@@ -15,69 +15,78 @@ Builder позволяет разделить процесс создания о
 делая код более гибким и удобным для использования.
  */
 
-// объявление data класса `Coffee` со свойствами `type`, `milk`, `sugar` и `size`.
-data class CoffeeBuilder(val type: String, val milk: Boolean, val sugar: Boolean, val size: String) {
-    // объявление вложенного класса `Builder`, который будет использоваться для создания объектов класса `Coffee` шаг за шагом.
-    class Builder {
-        private var type: String = ""
-        private var milk: Boolean = false
-        private var sugar: Boolean = false
-        private var size: String = ""
-
-        // объявление функции `setType`, которая устанавливает значение переменной `type` и возвращает текущий объект `Builder`
-        fun setType(type: String): Builder {
-            this.type = type
-            return this
-        }
-
-        // объявление функции `addMilk`, которая устанавливает значение переменной `milk` в `true` и возвращает текущий объект `Builder`.
-        fun addMilk(): Builder {
-            this.milk = true
-            return this
-        }
-
-        // объявление функции `addSugar`, которая устанавливает значение переменной `sugar` в `true` и возвращает текущий объект `Builder`.
-        fun addSugar(): Builder {
-            this.sugar = true
-            return this
-        }
-
-        // объявление функции `setSize`, которая устанавливает значение переменной `size` и возвращает текущий объект `Builder`
-        fun setSize(size: String): Builder {
-            this.size = size
-            return this
-        }
-
-        // объявление функции `build`, которая создает и возвращает объект класса `Coffee` на основе текущих значений переменных в `Builder`.
-        fun build(): CoffeeBuilder {
-            return CoffeeBuilder(type, milk, sugar, size)
-        }
-    }
-}
-
 fun main() {
     // создание экземпляра Builder и последовательное вызов его методов для установки значений свойств `type`, `sugar` и `size`,
     // а затем вызов метода `build()` для создания объекта `Coffee`.
-    val coffee = CoffeeBuilder.Builder()
-        .setType("Espresso")
-        .addSugar()
+    val coffee1 : Coffee11 = CoffeeBuilder()
+        .setType("Tea")
         .setSize("Small")
         .build()
 
-    val coffee2 = CoffeeBuilder.Builder()
+    val coffee2 : Coffee11 = CoffeeBuilder()
         .setType("Americano")
         .setSize("Middle")
         .addMilk()
         .build()
 
-    val coffee3 = CoffeeBuilder.Builder()
+    val coffee3 : Coffee11 = CoffeeBuilder()
         .setType("Latte")
         .setSize("Big")
         .addMilk()
         .addSugar()
         .build()
 
-    println(coffee)
+    println(coffee1)
     println(coffee2)
     println(coffee3)
+}
+
+// объявление data класса `Coffee` со свойствами `type`, `milk`, `sugar` и `size`.
+data class Coffee11(
+    val type: String,
+    val milk: Boolean,
+    val sugar: Boolean,
+    val size: String
+)
+
+// объявление класса `CoffeeBuilder`, который будет использоваться для создания объектов класса `Coffee` шаг за шагом.
+class CoffeeBuilder() {
+    var type: String = ""
+    var milk: Boolean = false
+    var sugar: Boolean = false
+    var size: String = ""
+
+    // объявление функции `setType`, которая устанавливает значение переменной `type` и возвращает текущий объект `Builder`
+    fun setType(value: String): CoffeeBuilder {
+        this.type = value
+        return this
+    }
+
+    // объявление функции `addMilk`, которая устанавливает значение переменной `milk` в `true` и возвращает текущий объект `Builder`.
+    fun addMilk(): CoffeeBuilder {
+        this.milk = true
+        return this
+    }
+
+    // объявление функции `addSugar`, которая устанавливает значение переменной `sugar` в `true` и возвращает текущий объект `Builder`.
+    fun addSugar(): CoffeeBuilder {
+        this.sugar = true
+        return this
+    }
+
+    // объявление функции `setSize`, которая устанавливает значение переменной `size` и возвращает текущий объект `Builder`
+    fun setSize(value: String): CoffeeBuilder {
+        this.size = value
+        return this
+    }
+
+    // объявление функции `build`, которая создает и возвращает объект класса `Coffee` на основе текущих значений переменных в `Builder`.
+    fun build(): Coffee11 {
+        return Coffee11(
+            type = type,
+            milk = milk,
+            sugar = sugar,
+            size = size
+        )
+    }
 }
--- src/main/kotlin/ru/otus/homework/homework6/Singleton.kt
+++ src/main/kotlin/ru/otus/homework/homework6/Singleton.kt
@@ -1,17 +1,33 @@
 package ru.otus.homework.homework6
 
+import kotlin.concurrent.thread
+
 // это шаблон проектирования, который гарантирует, что у класса есть только один экземпляр
 // и предоставляет глобальную точку доступа к этому экземпляру.
-object Singleton {
-    init {
-        println("Экземпляр Singleton инициализирован")
-    }
 
-    fun doSomething() {
-        println("Синглтон что-то делает")
+
+fun main() {
+    repeat(9999) {
+        thread {
+            println(Singleton.getSingleton())
+        }
     }
 }
 
-fun main() {
-    Singleton.doSomething() // вывод: Экземпляр Singleton инициализирован, Singleton что-то делает
+class Singleton private constructor() {
+    companion object {
+        private var instance: Singleton? = null
+
+        fun getSingleton(): Singleton {
+            if (instance == null) {
+                //проверка для многопоточного обращения
+                synchronized(Singleton::class.java) {
+                    if (instance == null) {
+                        instance = Singleton()
+                    }
+                }
+            }
+            return requireNotNull(instance)
+        }
+    }
 }
